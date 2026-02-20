648e6f0ed02683ea8f80135b9db4a653c84d51a8
Ratandeepsinghhh02 <rs0782@srmist.edu.in>
2026-02-20 10:23:23 +0530

    UC2: Added palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -7,6 +8,22 @@ public class PalindromeCheckerApp {
         System.out.printf("Hello and welcome to palindrome checker app");
 
         System.out.println("Version : 1.0");
+        Scanner sc=new Scanner(System.in);
+        String Text=sc.nextLine();
+        boolean isPalindrome=true;
+        for(int i=0;i<Text.length()/2;i++) {
+            if (Text.charAt(i) != Text.charAt(Text.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+
+            }
+
+        }
+        if(isPalindrome) {
+            System.out.println("It is a Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
 
     }
 }
