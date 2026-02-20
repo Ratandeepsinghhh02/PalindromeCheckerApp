54e8e4ccf2b04a9d35479388afef4de4380a8e97
Ratandeepsinghhh02 <rs0782@srmist.edu.in>
2026-02-20 10:36:16 +0530

    UC1:welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -6,7 +6,6 @@ public class PalindromeCheckerApp {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome to palindrome checker app");
 
-        System.out.println("Version : 1.0");
-
+        System.out.println("Version : 2.0");
     }
 }
