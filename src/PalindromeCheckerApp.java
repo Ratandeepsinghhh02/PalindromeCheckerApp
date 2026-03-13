8774d3b78fced8a5156770db707d96026a0916ed
Ratandeepsinghh02 <rs0782@srmist.edu.in>
2026-03-13 17:43:04 +0530

    added features uc9

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -218,5 +218,31 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println(inputLL + " is not a Palindrome");
         }
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters don't match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
+    }
+    String input = "madam";
+
+    boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        if (result) {
+        System.out.println(input + " is a Palindrome");
+    } else {
+        System.out.println(input + " is not a Palindrome");
+    }
+
+
 }
 
